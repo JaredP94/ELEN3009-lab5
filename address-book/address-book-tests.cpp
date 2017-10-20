605d550aa096df97e42027c9b594d19ba2450283
Jared <jaredping@yahoo.com>
2017-10-20 14:04:28 +0200

    Exercise 4.2

--- address-book/address-book-tests.cpp
+++ address-book/address-book-tests.cpp
@@ -16,3 +16,17 @@ TEST_CASE("Copy Constructed Object Does Not Share State With Original")
 
 	CHECK_FALSE(address1.getImageName() == address2.getImageName());
 }
+
+TEST_CASE("Object Copy Created Through Assignment Does Not Share State With Original")
+{
+	Person person1("Jonny",20);
+	Person person2("Sally",50);
+	
+	AddressBookEntry address1(person1,"person1 image");
+	AddressBookEntry address2 = address1;
+
+	address1.setPerson(person2);
+	address1.setImageName("person2 image");
+
+	CHECK_FALSE(address1.getImageName() == address2.getImageName());
+}
